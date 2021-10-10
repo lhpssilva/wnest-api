7be5e1863355ca9cde3eec59f8340654a6fe97c9
Luiz Silva <lhps.silva@outlook.com>
2021-10-10 19:50:47 -0300

    Feature/database (#1)
    * feature/database
    
    - Implement the database interface
    - Integrate devices route into the database
    
    * feature/database
    
    - Add category operations to the database interface
    - Integrate categories route into the database

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Joi = require('joi');
+const dbManager = require('../database/db');
 
 var mockList = [];
 
@@ -10,8 +11,18 @@ const categorySchema = Joi.object({
     .required()
 });
 
-router.get('/categories', (req, res) => {
-  res.send('get categories');
+router.get('/categories', async (req, res) => {
+  try {
+    let [ rows ] = await dbManager.getCategories();
+
+    if (rows && rows.length > 0)
+      res.status(200).send(JSON.stringify(rows));
+    else
+      res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Something went wrong');
+  }
 });
 
 router.post('/categories', async (req, res) => {
@@ -28,19 +39,26 @@ router.post('/categories', async (req, res) => {
   }
 
   try {
-    await insertIntoDatabase(value);
-    res.status(200).send(JSON.stringify(value));
+    await dbManager.insertNewCategory(value);
+    res.status(201)
+      .setHeader('location', `/categories`)
+      .send(JSON.stringify(value));
   } catch (err) {
     console.error(err);
     res.status(500).send('Something went wrong');
   }
 });
 
-async function insertIntoDatabase(value) {
-  return new Promise((resolve, reject) => {
-    mockList.push(value);
-    resolve();
-  });
-}
+router.delete('/categories/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await dbManager.deleteCategory(id);
+    res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Something went wrong');
+  }
+});
 
 module.exports = router;
